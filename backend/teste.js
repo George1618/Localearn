9f4a87036fa1c15a58b9a2c8f8b3276f525e1b36
Andre Cintra <andrebcintra4110@gmail.com>
2024-06-02 23:07:23 -0300

    Remoção do arquivo teste.js que era pra testar pegar as networks

--- backend/teste.js
+++
@@ -1,9 +0,0 @@
-const wifiScanner = require('node-wifi-scanner');
-
-wifiScanner.scan((err, networks) => {
-    if (err) {
-        console.error('Erro ao escanear redes Wi-Fi:', err);
-    } else {
-        console.log('Redes Wi-Fi detectadas:', networks);
-    }
-});
