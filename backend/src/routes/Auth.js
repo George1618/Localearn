857d3f7428d542ed58e1b37f7694532965b14e7a
RaifZeidam <mrzeidam2@gmail.com>
2024-06-23 21:17:55 -0300

    Atualização da autenticação

--- backend/src/routes/Auth.js
+++ backend/src/routes/Auth.js
@@ -9,20 +9,25 @@ admin.initializeApp({
 });
 
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+  const idToken = req.headers.authorization?.split(' ')[1];
+  console.log('Token JWT recebido:', idToken); // Verifica se o token é recebido corretamente
+
+  if (!idToken) {
+    return res.status(401).send({ error: 'Token não fornecido' });
+  }
 
   try {
-    // Autenticação com email e senha
-    const userCredential = await admin.auth().getUserByEmail(email);
-    const token = await admin.auth().createCustomToken(userCredential.uid);
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    const uid = decodedToken.uid;
+    console.log('Token JWT verificado:', uid); // Verifica se o token é verificado corretamente
 
     // Verifica em qual coleção o usuário está (professores ou alunos)
     let userData;
-    const professorDoc = await admin.firestore().collection('professores').doc(userCredential.uid).get();
+    const professorDoc = await admin.firestore().collection('professores').doc(uid).get();
     if (professorDoc.exists) {
       userData = professorDoc.data();
     } else {
-      const alunoDoc = await admin.firestore().collection('alunos').doc(userCredential.uid).get();
+      const alunoDoc = await admin.firestore().collection('alunos').doc(uid).get();
       if (alunoDoc.exists) {
         userData = alunoDoc.data();
       } else {
@@ -30,40 +35,44 @@ router.post('/login', async (req, res) => {
       }
     }
 
-    res.status(200).send({ token, userData });
+    res.status(200).send({ token: idToken, userData });
   } catch (error) {
     res.status(400).send({ error: error.message });
   }
 });
 
 router.post('/signup', async (req, res) => {
-    const { email, password, username, isTeacher } = req.body;
-  
-    try {
-      const userCredential = await admin.auth().createUser({
-        email: email,
-        password: password,
-        displayName: username
-      });
-  
-      const user = userCredential;
-  
-      const userData = {
-        email: user.email,
-        nome: username,
-      };
-  
-      const collection = isTeacher ? 'professores' : 'alunos';
-      await admin.firestore().collection(collection).doc(user.uid).set(userData);
-  
-      res.status(201).send({ message: 'Usuário criado com sucesso!' });
-    } catch (error) {
-      res.status(400).send({ error: error.message });
-    }
-  });
+  const { email, password, username, isTeacher } = req.body;
+
+  try {
+    const userCredential = await admin.auth().createUser({
+      email: email,
+      password: password,
+      displayName: username
+    });
+
+    const user = userCredential;
+
+    const userData = {
+      email: user.email,
+      nome: username,
+    };
+
+    const collection = isTeacher ? 'professores' : 'alunos';
+    await admin.firestore().collection(collection).doc(user.uid).set(userData);
+
+    res.status(201).send({ message: 'Usuário criado com sucesso!' });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
 
 router.get('/checkAuth', async (req, res) => {
-  const idToken = req.headers.authorization;
+  const idToken = req.headers.authorization?.split(' ')[1];
+
+  if (!idToken) {
+    return res.status(401).send({ error: 'Token não fornecido' });
+  }
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
